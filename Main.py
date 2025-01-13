10f0eb0292fe58b8e0c5a6b9088d04460535d91f
cheniva-mvla <100029088@mvla.net>
2025-01-13 11:08:49 -0800

    refactored code

--- Main.py
+++ Main.py
@@ -7,16 +7,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)        
 GPIO.setup(pin, GPIO.OUT)   
 
-def turnOnPower():
-    GPIO.output(pin, GPIO.HIGH)  
-    print("Powering Pin " + str(pin))      
-
-def turnOffPower():
-    GPIO.output(pin, GPIO.LOW)    
-    print("Unpowering Pin " + str(pin))      
+def TurnPowerOnAndOff(bool):
+    if bool: 
+        GPIO.output(pin, GPIO.HIGH)  
+        print("Powering Pin " + str(pin))      
+    else:
+        GPIO.output(pin, GPIO.LOW)    
+        print("Unpowering Pin " + str(pin))    
 
 while True:                         
-    turnOnPower()
+    TurnPowerOnAndOff(True)
     sleep(1)     
-    turnOffPower()
+    TurnPowerOnAndOff(False)
     sleep(1)
