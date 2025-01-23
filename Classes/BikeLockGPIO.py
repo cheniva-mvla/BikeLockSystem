a05024b61b147dc3464a0db4974290e8e88924f9
chungsar-mvla <141953285+chungsar-mvla@users.noreply.github.com>
2025-01-23 11:30:41 -0800

    added on off functions and toggle

--- Classes/BikeLockGPIO.py
+++ Classes/BikeLockGPIO.py
@@ -11,11 +11,23 @@ class BLGPIO:
     def __init__(self, pins):
         '''
         GPIO.setmode(GPIO.BCM)         
-        GPIO.setwarnings(False)        
-        GPIO.setup(self.pin, GPIO.OUT)       
+        GPIO.setwarnings(False)       
         '''
+        self.pins = pins
+        # GPIO.setup(pins, GPIO.OUT, GPIO.LOW)
+
+    
     def __str__(self):
         return "I am GPIO"
+
+    def turnOn(pin):
+        GPIO.output(pin, GPIO.HIGH)
+
+    def turnOff(pin):
+        GPIO.output(pin,GPIO.LOW)
+
+    def toggle(pin):
+        GPIO.output(pin, not GPIO.input(pin))
               
 
 
