d8b8e02de106eb60b39fc8d1fb36b5697125a3f2
cheniva-mvla <100029088@mvla.net>
2025-03-25 10:59:31 -0700

    rfid fixed, testing main loop

--- BikeLockDriver.py
+++ BikeLockDriver.py
@@ -173,9 +173,9 @@ def BikeLockSystem():
             canUnlock = True
             print("Bike can be unlocked")
 
-BLRFID.readRFID(BLRFID)
+#BLRFID.readRFID(BLRFID)
 
-#BikeLockSystem()
+BikeLockSystem()
 #printInfo()
 #reportPinConnectivity()
 #trigger()
